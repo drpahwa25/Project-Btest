545fbb1b32ed2624c7bc70bcb6aa356621da0f8b
shwet <shwet171099@gmail.com>
2024-08-12 12:16:27 +1000

    Remove node_modules and add to .gitignore

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Sample endpoints
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.get("/api/data", (req, res) => {
+  res.json({ message: "Here is some data!" });
+});
+
+app.post("/api/data", (req, res) => {
+  const receivedData = req.body;
+  res.json({
+    message: "Data received successfully!",
+    data: receivedData,
+  });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
